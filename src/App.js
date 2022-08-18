14f96469440fb49270cd148d7930b4b2b0fb2774
Matheus <matheusrelvinhas@gmail.com>
2022-08-18 14:45:30 -0300

    primeiroCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import getPlanetsAPI from './functions/getPlanetsAPI';
+import starWarsContext from './context/starWarsContext';
+import getResidents from './functions/getResidents';
+import Table from './components/Table';
+import alphabeticalOrder from './functions/alphabeticalOrder';
 
 function App() {
+  const [APIReturn, setAPIReturn] = useState({});
+  const [nameForSearch, setNameForSearch] = useState('');
+  const [filterByNumericValues, setFilterByNumericValues] = useState([{
+    column: '',
+    comparison: '',
+    value: 0,
+  }]);
+  const [columnOptions, setColumnOptions] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
+  const [order, setOrder] = useState({
+    column: 'population',
+    sort: 'ASC',
+  });
+
+  useEffect(() => {
+    async function handleFetch() {
+      const planetsObject = await getPlanetsAPI('https://swapi-trybe.herokuapp.com/api/planets/');
+      getResidents(planetsObject.results);
+      alphabeticalOrder(planetsObject.results);
+      setAPIReturn(planetsObject);
+    }
+    handleFetch();
+  }, []);
+
+  const contextValue = {
+    data: APIReturn,
+    setAPIReturn,
+    changeData: setAPIReturn,
+    filterByName: {
+      name: nameForSearch,
+    },
+    setNameForSearch,
+    filterByNumericValues,
+    setFilterByNumericValues,
+    columnOptions,
+    setColumnOptions,
+    order,
+    setOrder,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <starWarsContext.Provider value={ contextValue }>
+        {
+          Object.keys(APIReturn).length !== 0 && (
+            <Table />
+          )
+        }
+      </starWarsContext.Provider>
     </div>
   );
 }
